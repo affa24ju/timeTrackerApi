cb2d9ebca0cb9409f51723f209e895a550a34e23
Fatema Afroza <fafroza2017@gmail.com>
2025-04-28 15:47:14 +0200

    First commit

---
+++ src/main/java/com/timeTrackerAPI/timeTrackerAPI/TimeTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.timeTrackerAPI.timeTrackerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeTrackerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timeTrackerAPI/timeTrackerAPI/TimeTrackerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeTrackerAPI.timeTrackerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeTrackerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
