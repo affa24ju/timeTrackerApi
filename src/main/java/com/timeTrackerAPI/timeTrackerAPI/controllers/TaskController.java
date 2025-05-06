26fde4ce8c7d90928a37ba32698810d087267016
Fatema Afroza <fafroza2017@gmail.com>
2025-05-06 12:37:16 +0200

    Skapat ny getrequest, visar stats för en vald vecka

--- src/main/java/com/timeTrackerAPI/timeTrackerAPI/controllers/TaskController.java
+++ src/main/java/com/timeTrackerAPI/timeTrackerAPI/controllers/TaskController.java
@@ -1,8 +1,13 @@
 package com.timeTrackerAPI.timeTrackerAPI.controllers;
 
+import java.time.DayOfWeek;
+import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.temporal.TemporalAdjusters;
+import java.time.temporal.WeekFields;
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -14,6 +19,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.timeTrackerAPI.timeTrackerAPI.models.Category;
@@ -61,7 +67,7 @@ public class TaskController {
         return taskRepository.findByStartTimeBetween(startOfWeek, endOfWeek);
     }
 
-    //Visa statistik på hur många minuter per task i veckan
+    //Visa statistik på hur många minuter per task denna veckan
     @GetMapping("/stats/week")
     public List<WeeklyCategoryStat> getWeeklyStats(){
         LocalDateTime now = LocalDateTime.now();
@@ -92,6 +98,53 @@ public class TaskController {
             })
             .collect(Collectors.toList());
 
+    }
+    //Hämta statistik för en vald vecka, t.ex. /api/tasks/stats/week?year=2025 
+    @GetMapping("/stats/specificweek")
+    public List<WeeklyCategoryStat> getWeeklyStatsByYearAndWeek(@RequestParam int year, @RequestParam int week){
+        //Skapar en kalender för att räkna ut veckans start & slut
+        WeekFields weekFields = WeekFields.of(Locale.getDefault());
+
+        //Startar med måndag i önskad vecka
+        LocalDate startOfWeekDate = LocalDate
+            .now()
+            .withYear(year)
+            .with(weekFields.weekOfYear(), week)
+            .with(TemporalAdjusters.previousOrSame(DayOfWeek.MONDAY));
+        
+        //Slutar på söndag samma vecka
+        LocalDate endOfWeekDate = startOfWeekDate.with(TemporalAdjusters.nextOrSame(DayOfWeek.SUNDAY));
+
+        //Gör om LocatDate till LocalDateTime för att inkludera tid
+        LocalDateTime startOfWeek = startOfWeekDate.atStartOfDay();
+        LocalDateTime endOfWeek = endOfWeekDate.atTime(23, 59);
+
+        //Hämtar alla tasks som startade mellan mån 00:00 & sön 23:59
+        List<Task> tasks = taskRepository.findByStartTimeBetween(startOfWeek, endOfWeek);
+
+        //Grupperar & summerar antal minuter per kategori-id
+        Map<String, Long> minutesPerCategoryId = tasks.stream()
+            .filter(task -> task.getEndTime() != null) //filtrear bort tasks som inte är avslutad
+            .collect(Collectors.groupingBy(
+                Task::getCategoryId, //Gruperar på kategoriID
+                Collectors.summingLong(task -> 
+                    Duration.between(task.getStartTime(), task.getEndTime()).toMinutes() //summerar tid i minuter
+                )
+            ));
+
+        //Konverterar från kategoriId till categoryName & bygger weeklyCategoryStat objekt per kategori
+        return minutesPerCategoryId.entrySet().stream()
+            .map(entry -> {
+                String categoryId = entry.getKey();
+                //Hämtar kategorinamn från category repository
+                String categoryName = categoryRepository.findById(categoryId)
+                    .map(Category::getName)
+                    .orElse("Ökänd kategori");
+                
+                //Returnerar ett objekt med kategorynamn & minuter
+                return new WeeklyCategoryStat(categoryName, entry.getValue());
+            })
+            .collect(Collectors.toList());
     }
 
 }
